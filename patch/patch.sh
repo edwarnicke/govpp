b8874652e7d48e40f104ffd33ea9b7f68e29cac1
Ed Warnicke <hagbard@gmail.com>
2022-10-20 20:20:04 -0500

    Update to vpp 22.10-rc2
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

--- patch/patch.sh
+++ patch/patch.sh
@@ -12,6 +12,22 @@ function git_cherry_pick ()
 	git commit --amend -m "gerrit:${refs#refs/changes/*/} $(git log -1 --pretty=%B)"
 }
 
+# Calico cherry picks
+git_cherry_pick refs/changes/13/34713/4 # 34713: vppinfra: improve & test abstract socket | https://gerrit.fd.io/r/c/vpp/+/34713
+git_cherry_pick refs/changes/71/32271/16 # 32271: memif: add support for ns abstract sockets | https://gerrit.fd.io/r/c/vpp/+/32271
+git_cherry_pick refs/changes/34/34734/3 # 34734: memif: autogenerate socket_ids | https://gerrit.fd.io/r/c/vpp/+/34734
+git_cherry_pick refs/changes/26/34726/1 # 34726: interface: add buffer stats api | https://gerrit.fd.io/r/c/vpp/+/3472
+
+# --------------- Dedicated plugins ---------------
+git_cherry_pick refs/changes/64/33264/7 # 33264: pbl: Port based balancer | https://gerrit.fd.io/r/c/vpp/+/33264
+git_cherry_pick refs/changes/88/31588/4 # 31588: cnat: [WIP] no k8s maglev from pods | https://gerrit.fd.io/r/c/vpp/+/31588
+git_cherry_pick refs/changes/83/28083/21 # 28083: acl: acl-plugin custom policies | https://gerrit.fd.io/r/c/vpp/+/28083
+git_cherry_pick refs/changes/13/28513/28 # 25813: capo: Calico Policies plugin | https://gerrit.fd.io/r/c/vpp/+/28513
+# --------------- Dedicated plugins ---------------
+
+# NSM cherry-picks
+git_cherry_pick refs/changes/41/37141/11 # 37141: l2: Add bridge_domain_add_del_v2 to l2 api | https://gerrit.fd.io/r/c/vpp/+/37141
+
 if [ "$(ls ./patch/*.patch 2> /dev/null)" ]; then
   git apply patch/*.patch
   git add --all
