ffd310818ddaa06b11a5ded73d3f868d2e8027fb
Artem Glazychev <artem.glazychev@xored.com>
2023-01-24 18:39:33 +0700

    Update vpp to e416893a597959509c7f667c140c271c0bb78c14
    Signed-off-by: Artem Glazychev <artem.glazychev@xored.com>

--- patch/patch.sh
+++ patch/patch.sh
@@ -13,27 +13,24 @@ function git_cherry_pick ()
 }
 
 # Calico cherry picks
-git_cherry_pick refs/changes/12/33312/4 # 33312: sr: fix srv6 definition of behavior associated to a LocalSID | https://gerrit.fd.io/r/c/vpp/+/33312
-git_cherry_pick refs/changes/13/34713/3 # 34713: vppinfra: improve & test abstract socket | https://gerrit.fd.io/r/c/vpp/+/34713
-git_cherry_pick refs/changes/71/32271/15 # 32271: memif: add support for ns abstract sockets | https://gerrit.fd.io/r/c/vpp/+/32271
-git_cherry_pick refs/changes/34/34734/2 # 34734: memif: autogenerate socket_ids | https://gerrit.fd.io/r/c/vpp/+/34734
+git_cherry_pick refs/changes/13/34713/4 # 34713: vppinfra: improve & test abstract socket | https://gerrit.fd.io/r/c/vpp/+/34713
+git_cherry_pick refs/changes/71/32271/16 # 32271: memif: add support for ns abstract sockets | https://gerrit.fd.io/r/c/vpp/+/32271
+git_cherry_pick refs/changes/34/34734/3 # 34734: memif: autogenerate socket_ids | https://gerrit.fd.io/r/c/vpp/+/34734
 git_cherry_pick refs/changes/26/34726/1 # 34726: interface: add buffer stats api | https://gerrit.fd.io/r/c/vpp/+/34726
-git_cherry_pick refs/changes/52/34852/2 # 34852: memif: fix rx/txqueue RC on connected | https://gerrit.fd.io/r/c/vpp/+/34852
-git_cherry_pick refs/changes/09/35209/1 # 35209: cnat: Fix conflicting rsession | https://gerrit.fd.io/r/c/vpp/+/35209
-git_cherry_pick refs/changes/39/35439/1 # 35439: ip: fix assert in ip4_ttl_inc | https://gerrit.fd.io/r/c/vpp/+/35439
-git_cherry_pick refs/changes/38/35438/1 # 35438: af_packet: fix tx stall by retrying failed sendto | https://gerrit.fd.io/r/c/vpp/+/35438
+git_cherry_pick refs/changes/05/35805/2 # 35805: dpdk: add intf tag to dev{} subinput | https://gerrit.fd.io/r/c/vpp/+/35805
 
 # --------------- Dedicated plugins ---------------
 git_cherry_pick refs/changes/64/33264/7 # 33264: pbl: Port based balancer | https://gerrit.fd.io/r/c/vpp/+/33264
-git_cherry_pick refs/changes/88/31588/1 # 31588: cnat: [WIP] no k8s maglev from pods | https://gerrit.fd.io/r/c/vpp/+/31588
+git_cherry_pick refs/changes/88/31588/4 # 31588: cnat: [WIP] no k8s maglev from pods | https://gerrit.fd.io/r/c/vpp/+/31588
 git_cherry_pick refs/changes/83/28083/21 # 28083: acl: acl-plugin custom policies |  https://gerrit.fd.io/r/c/vpp/+/28083
-git_cherry_pick refs/changes/13/28513/25 # 25813: capo: Calico Policies plugin | https://gerrit.fd.io/r/c/vpp/+/28513
+git_cherry_pick refs/changes/13/28513/28 # 25813: capo: Calico Policies plugin | https://gerrit.fd.io/r/c/vpp/+/28513
 # --------------- Dedicated plugins ---------------
 
 # NSM cherry-picks
-git_cherry_pick refs/changes/45/36945/2 # 36945: wireguard: fix ipv6 handshake packet | https://gerrit.fd.io/r/c/vpp/+/36945
-git_cherry_pick refs/changes/18/37018/1 # 37018: wireguard: fix ipv6 payload_length computation | https://gerrit.fd.io/r/c/vpp/+/37018
-git_cherry_pick refs/changes/57/36157/3 # 36157: ikev2: fix tunnel direction | https://gerrit.fd.io/r/c/vpp/+/36157
+git_cherry_pick refs/changes/74/37274/5 # 37274: af_xdp: fix xdp socket create fail | https://gerrit.fd.io/r/c/vpp/+/37274
+git_cherry_pick refs/changes/63/37763/4 # 37763: wireguard: add local variable | https://gerrit.fd.io/r/c/vpp/+/37763
+git_cherry_pick refs/changes/01/38001/9 # 38001: wireguard: sending the first handshake | https://gerrit.fd.io/r/c/vpp/+/38001
+git_cherry_pick refs/changes/00/38000/4 # 38000: wireguard: update ESTABLISHED flag | https://gerrit.fd.io/r/c/vpp/+/38000
 
 if [ "$(ls ./patch/*.patch 2> /dev/null)" ]; then
   git apply patch/*.patch
